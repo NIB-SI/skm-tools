b1776759c28046a4e7311c0bdb8d69965bdedd9b
carissableker <carissableker@users.noreply.github.com>
2024-07-09 11:52:38 +0200

    update PSS example

--- skm_tools/load_networks.py
+++ skm_tools/load_networks.py
@@ -8,7 +8,7 @@ import pandas as pd
 from .skm_download_urls import *
 
 def pss_to_networkx(edge_path=None, node_path=None):
-    ''' Load PSS from the rxn SIF format to a
+    ''' Load PSS from the rxn bipartite projection SIF format to a
     networkx directed multigraph format, including node attributes
 
     Parameters
@@ -81,6 +81,104 @@ def pss_to_networkx(edge_path=None, node_path=None):
     return g
 
 
+
+def pss_model_to_networkx(edge_path=None, node_path=None):
+    ''' Load PSS from the main SIF format to a
+    networkx directed multigraph format, including node attributes
+
+    Parameters
+    ----------
+
+    edge_path : str or pathlib.Path
+        Path to the edge list file,
+        if file does not exist, download from skm.nib.si
+
+    node_path : str or pathlib.Path
+        Path to the node annotation file,
+        if file does not exist, download from skm.nib.si
+
+    '''
+    edge_path = Path(edge_path)
+    node_path = Path(node_path)
+
+    if not edge_path.exists():
+        print(f"Attempting to download the edge list to {edge_path}.", end=" ")
+        urlretrieve(PSS_EDGE_URL, edge_path)
+        print("Success.")
+
+    if not node_path.exists():
+        print(f"Attempting to download the node annotations to {node_path}.", end=" ")
+        urlretrieve(PSS_NODE_URL, node_path)
+        print("Success.")
+
+    with open(edge_path, "rb") as handle:
+        handle.readline()
+        g = nx.read_edgelist(handle,
+                    delimiter="\t", create_using=nx.MultiDiGraph,
+                        data=list({
+                            'interaction_type':str,
+                            'directed': str,
+                            'reaction_type': str,
+                            'reaction_effect': str,
+                            'reaction_id': str,
+                            'source_edge_type': str,
+                            'source_location': str,
+                            'source_form': str,
+                            'target_edge_type': str,
+                            'target_location': str,
+                            'target_form': str,
+                        }.items()))
+
+    node_df = pd.read_csv(node_path, sep="\t")
+    node_df.set_index("name", inplace=True, drop=False)
+
+    def to_list(x):
+        if isinstance(x, str):
+            return x.split(',')
+        return None
+
+    for c in [
+        'ath_homologues',
+        'osa_homologues',
+        'stu_homologues',
+        'sly_homologues'
+        ]:
+        node_df[c] = node_df[c].apply(to_list)
+
+    nx.set_node_attributes(g, node_df.to_dict('index'))
+
+    for _, data in g.nodes(data=True):
+        if data["node_type"] in ("PlantCoding", "PlantAbstract", "PlantNonCoding"):
+            data["display_label"] = data["short_name"]
+        else:
+            data["display_label"] = data["name"]
+
+    return g
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def ckn_to_networkx(
         edge_path=None,
         node_path=None,
@@ -119,8 +217,6 @@ def ckn_to_networkx(
     directed : bool
         Whether to remove undirected edges from CKN
         (in contrast to add_reciprocal_edges, and not meant to be used together)
-
-
     '''
     edge_path = Path(edge_path)
     node_path = Path(node_path)
--- skm_tools/skm_download_urls.py
+++ skm_tools/skm_download_urls.py
@@ -1,5 +1,11 @@
 '''URLs for downloading networks from skm.nib.si'''
 
+# PSS Model
+PSS_EDGE_URL = 'https://skm.nib.si/downloads/pss/public/sif-edges'
+PSS_NODE_URL = 'https://skm.nib.si/downloads/pss/public/sif-nodes'
+
+
+# PSS Projection
 PSS_RXN_EDGE_URL = 'https://skm.nib.si/downloads/pss/public/rxn-edges'
 PSS_RXN_NODE_URL = 'https://skm.nib.si/downloads/pss/public/rxn-nodes'
 
